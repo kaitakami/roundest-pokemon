cacc18557cc9f6b7c358e2e91b53ba18dccb3ae2
Kai Takami <kaitakami@protonmail.com>
2023-02-05 19:58:51 +0900

    Create router for pokemon with trpc

--- src/server/api/routers/pokemon.ts
+++ src/server/api/routers/pokemon.ts
@@ -1,16 +1,32 @@
 import { z } from "zod";
-import { PokemonClient } from "pokenode-ts";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const pokemonRouter = createTRPCRouter({
   "get-pokemon-by-id": publicProcedure
-    .input(z.object({ id: z.number() }))
-    .query(async ({ input }) => {
-      const api = new PokemonClient();
-      const pokemon = await api.getPokemonById(input.id);
-      return { name: pokemon.name, sprites: pokemon.sprites };
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const pokemon = await ctx.prisma.pokemon.findFirst({
+        where: {
+          id: input.id,
+        },
+      });
+
+      if (!pokemon) throw new Error("pokemon doesn't exist");
+      return pokemon;
     }),
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
+  "cast-vote": publicProcedure
+    .input(
+      z.object({
+        votedAgainstId: z.string(),
+        votedForId: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const voteInDb = await ctx.prisma.vote.create({
+        data: {
+          ...input,
+        },
+      });
+      return { success: true, vote: voteInDb };
+    }),
 });
