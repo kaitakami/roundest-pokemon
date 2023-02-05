4a3af0a6866efef4edb1e6276720d290499b4032
Kai Takami <kaitakami@protonmail.com>
2023-02-05 19:59:07 +0900

    Create function to generate random number (pokemon)

--- src/utils/getRandomPokemon.ts
+++ src/utils/getRandomPokemon.ts
@@ -1,17 +1,17 @@
 const MAX_DEX_ID = 493;
 
-const getRandomPokemon: (notThisOne?: number) => number = (
-  notThisOne?: number
+const getRandomPokemon: (notThisOne?: string) => string = (
+  notThisOne?: string
 ) => {
-  const pokedexNumber = Math.floor(Math.random() * MAX_DEX_ID) + 1;
+  const pokedexNumber = String(Math.floor(Math.random() * MAX_DEX_ID + 1));
 
   if (pokedexNumber !== notThisOne) return pokedexNumber;
   return getRandomPokemon(notThisOne);
 };
 
-export const getOptionsForVote: () => [number, number] = () => {
-  const firstId = getRandomPokemon();
-  const secondId = getRandomPokemon(firstId);
+export const getOptionsForVote: () => [string, string] = () => {
+  const firstId = String(getRandomPokemon());
+  const secondId = String(getRandomPokemon(firstId));
 
   return [firstId, secondId];
 };
